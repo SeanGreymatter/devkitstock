8163d15114190eb621cf992405b37b9fe5943481
SeanGreymatter <76600865+SeanGreymatter@users.noreply.github.com>
2024-05-23 06:48:43 +0100

    Buttons and label additions

--- StockCtrl/tktutorial.py
+++ StockCtrl/tktutorial.py
@@ -4,19 +4,27 @@ headertxt = ("arial", 12, "bold")
 buttontxt = ("arial", 8, "bold")
 
 window = Tk()
-# window.geometry("300x300")
+window.geometry("300x300")
 window.title("Hello World")
 
-frame = Frame(window, padx=50, pady=50)
-frame.grid(row = 0, column = 0, sticky = "nsew")
-window.grid_rowconfigure(0, weight=1)
-window.grid_columnconfigure(0, weight=1)
+# frame = Frame(window, padx=500, pady=500)
+# frame.grid(row = 0, column = 0, sticky = "nsew",)
+# window.grid_rowconfigure(0, weight=1)
+# window.grid_columnconfigure(0, weight=1)
+
+def printt():
+    print("TKinter Tutorial")
+
+def exit_window():
+    exit()
 
 label_1 = Label(window, text = "Welcome to the World", font = headertxt)
-label_1.grid(row=1, column=1, padx=10, pady=10)
+label_1.pack()
 
-button1 = Button(window,text="Button", font = buttontxt)
-button1.grid(row=1, column=0, padx=10, pady=10)
+button1 = Button(window,text="Submit", font = buttontxt, command = printt)
+button1.place(x=100, y=100)
 
+button2 = Button(window,text="Close", font = buttontxt, command = exit_window)
+button2.place(x=150, y=100)
 
 window.mainloop()
